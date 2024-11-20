8142203faf8d1e6ad37cb76e005247717ed3eb1e
SahnounHoussem <sahnoun.houssem05@gmail.com>
2024-11-21 00:25:15 +0100

    modify cv

--- src/data/bio.js
+++ src/data/bio.js
@@ -8,6 +8,6 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/SAHNOUN-HOUSSEM",
   resume:
-    "https://drive.google.com/file/d/1FahySB89sdjCNEFnH7q3vccONW_mCUtV/view?usp=sharing",
+    "https://drive.google.com/file/d/1ov70ekIeUC6vWKBVyaDtojYeb5cOlGDb/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/sahnoun-houssem/",
 };
